8e6e54b77c853a7e1a1c5cd8b7e15e76a2ff46d9
Dennis Obel <ogembodenis2016@gmail.com>
2018-01-20 06:37:55 +0300

    socketio tweaks still

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+/*
 var express = require('express');
 var app = express();
 var server = require('http').createServer(app);
@@ -81,3 +82,64 @@ var port = process.env.PORT || 8000;
 server.listen(port,function(){
 	console.log("listening in http://localhost:" + port);
 })
+*/
+
+var server = require("http").createServer(onRequest);
+var io = require("socket.io")(server);
+
+function onRequest(req,res){
+	res.writeHead(200, {
+	'Access-Control-Allow-Origin' : '*'
+	});
+};
+
+io.on('connection',function(socket){
+	socket.emit("connected",clients)
+	
+	//console.log(clients)
+	console.log("a new client has connected with the id " + socket.id + "!");
+
+	socket.on("getconnectedusers",()=>{
+		io.emit("emitconnectedusers",clients)
+	})
+
+	socket.on("add_user",(data)=>{		
+		clients[data.username]	={
+			"socket":socket.id
+		};			
+		//console.log(clients)
+		// io.emit("connected",clients)
+	});
+
+	socket.on("add-cart",(cart)=>{	
+		io.emit("live",clients)
+		console.log(cart.socket)
+		for(key in clients){	
+			this.isequal = cart.socket == clients[key].socket
+			console.log(this.isequal)
+		}
+		if(isequal == true){	
+			console.log("about to emit data to companion...")
+			io.sockets.connected[cart.socket].emit("new_order",cart,()=>{
+				console.log("emitting data to companion")
+			})
+		}else{
+			console.log("do na'en")
+		}
+	});
+
+	// socket.on('end', function (){
+ //    	io.disconnect(0);
+	// });
+
+	socket.on("disconnect",()=>{
+		io.emit("users-changed",{user: socket.nickname, event: "left"});		
+	});
+})
+
+var port = process.env.PORT || 8000;
+
+server.listen(port,function(){
+	console.log("listening in http://localhost:" + port);
+})
+
