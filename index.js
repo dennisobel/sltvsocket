cae810f96ed15e4f124c0d4524587c4a6b69c8c4
Dennis Obel <ogembodenis2016@gmail.com>
2018-01-21 03:14:13 +0300

    socketcor

--- index.js
+++ index.js
@@ -1,37 +1,33 @@
-/*
-var express = require('express');
-var app = express();
+var app = require('express')();
+//var app = express();
 var server = require('http').createServer(app);
 // var socketio = require('socket.io');
 // var io = socketio().listen(server);
-var io = require("socket.io").listen(server)
+var io = require("socket.io")(server);
 
 // var mongoose = require("mongoose")
 // var MongoClient = require('mongodb').MongoClient;
 var db
 var clients = {};
 var isequal = true;
-var cors = require('cors');
+//var cors = require('cors');
 
-app.use(cors());
-
-
-
-app.use(function(req,res,next){
-    var allowedOrigins = ["http://localhost:8100","http://localhost:8101","https://sltvsocket.herokuapp.com/","http://localhost:8080/cart/createcart","http://localhost:8080/cart/getcart",'http://localhost:8080/api/auth/protected','http://localhost:8080/api/auth/register','http://localhost:8080/api/auth/login'];
-    var origin = req.headers.origin;
-    if(allowedOrigins.indexOf(origin) > -1){
-        res.setHeader("Access-Control-Allow-Origin", origin);
-    }
-	res.setHeader("Access-Control-Allow-Methods", "POST, PUT, OPTIONS, DELETE, GET");    
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    res.header("Access-Control-Allow-Credentials", true);
-    return next();
-})
+// app.use(cors());
+// app.use(function(req,res,next){
+//     var allowedOrigins = ["http://localhost:8100","http://localhost:8101","https://sltvsocket.herokuapp.com/","http://localhost:8080/cart/createcart","http://localhost:8080/cart/getcart",'http://localhost:8080/api/auth/protected','http://localhost:8080/api/auth/register','http://localhost:8080/api/auth/login'];
+//     var origin = req.headers.origin;
+//     if(allowedOrigins.indexOf(origin) > -1){
+//         res.setHeader("Access-Control-Allow-Origin", origin);
+//     }
+// 	res.setHeader("Access-Control-Allow-Methods", "POST, PUT, OPTIONS, DELETE, GET");    
+//     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//     res.header("Access-Control-Allow-Credentials", true);
+//     return next();
+// })
 
 
 var allowedOrigins = ["http://localhost:8100","http://localhost:8101","https://sltvsocket.herokuapp.com/","http://localhost:8080/cart/createcart","http://localhost:8080/cart/getcart",'http://localhost:8080/api/auth/protected','http://localhost:8080/api/auth/register','http://localhost:8080/api/auth/login'];
-io.set("origins",allowedOrigins)
+io.origins(allowedOrigins)
 
 io.on('connection',function(socket){
 	socket.emit("connected",clients)
@@ -82,14 +78,16 @@ var port = process.env.PORT || 8000;
 server.listen(port,function(){
 	console.log("listening in http://localhost:" + port);
 })
-*/
+
+
+/*
 
 var server = require("http").createServer(onRequest);
 var io = require("socket.io")(server);
 
 function onRequest(req,res){
 	res.writeHead(200, {
-	'Access-Control-Allow-Origin' : '*'
+	'Access-Control-Allow-Origin' : 'http://localhost/'
 	});
 };
 
@@ -143,3 +141,4 @@ server.listen(port,function(){
 	console.log("listening in http://localhost:" + port);
 })
 
+*/
